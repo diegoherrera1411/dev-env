5f1a71a152d5e7fc3e8f7efcf6d2028156f4581d
diegoherrera1411 <diego.herrera@nubank.com.br>
2023-12-05 11:47:17 -0500

    Bugifx: allow external connections to kafka instance (advertised.listeners)

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 The idea of this repo is to have an easy way to set up datomic database
 and a Kafka cluster for rapid prototyping in development
 
-## Current status
+## Current status
 
 * Deploys Datomic using local dev set up
 * Depploys a Kafka cluster with one broker
--- kafka/start-kafka.sh
+++ kafka/start-kafka.sh
@@ -3,5 +3,9 @@
 # Replace config values un properties file
 perl -pi -e "s/zookeeper.connect=localhost/zookeeper.connect=$ZOOKEEPER_HOST/g" /kafka/config/kafka.properties
 perl -pi -e "s/broker.id=0/broker.id=$BROKER_ID/g" /kafka/config/kafka.properties
+# Allow external connections to the Kafka instance
+perl -pi -e "s/#advertised.listeners=PLAINTEXT:\/\/your.host.name:9092/advertised.listeners=PLAINTEXT:\/\/localhost:9092/g" /kafka/config/kafka.properties
+# Change log directory in case of instance restart
+perl -pi -e "s/log.dirs=\/tmp\/kafka-logs/log.dirs=\/kafka\/kafka-logs/g" /kafka/config/kafka.properties
 
 /kafka/bin/kafka-server-start.sh /kafka/config/kafka.properties
